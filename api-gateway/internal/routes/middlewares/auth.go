e70408f612c9faf9e8f06620c2b1ed748fc38b74
kibet <kmishmael@gmail.com>
2024-03-21 17:43:16 +0300

    activate automigrate and synchronization for all models and use production secret to validate jwt bearer tokens

--- api-gateway/internal/routes/middlewares/auth.go
+++ api-gateway/internal/routes/middlewares/auth.go
@@ -1,7 +1,9 @@
 package middlewares
 
 import (
+	"fmt"
 	"net/http"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 	"github.com/kmishmael/sensorgrove/pkg/tokens"
@@ -10,17 +12,27 @@ import (
 func Authenticate(context *gin.Context) {
 	token := context.Request.Header.Get("Authorization")
 
+	fmt.Println(token)
+
 	if token == "" {
-		context.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"message": "Not Authorized."})
+		context.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"message": "Authorization header is missing"})
 		return
 	}
 
-	userId, err := tokens.VerifyToken(token)
+	parts := strings.Split(token, " ")
+    if len(parts) != 2 || parts[0] != "Bearer" {
+		context.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid authorization header"})
+        return
+    }
+	tokenSection := parts[1]
+
+	claims, err := tokens.VerifyToken(tokenSection)
 
 	if err != nil {
 		context.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"message": "Not Authorized."})
 		return
 	}
-	context.Set("userId", userId)
+
+	context.Set("user", claims)
 	context.Next()
 }
