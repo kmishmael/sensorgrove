abc2df13d5262d48cb9eb6b10fb95da40c1e53da
kibet <kmishmael@gmail.com>
2024-03-29 11:18:48 +0300

    chore: product page ui

--- client/app/product/page.tsx
+++ client/app/product/page.tsx
@@ -1,17 +1,45 @@
+import CategoriesButtons from "@/components/categories-buttons";
 import ProductGallery from "@/components/gallery";
 import NavBar from "@/components/navbar";
 import ProductTitle from "@/components/product";
+import ProductDescription from "@/components/product-description";
+import { RiArrowRightSLine } from "react-icons/ri";
+import { Tab } from "@headlessui/react";
 
 export default function Page() {
   return (
     <>
       <NavBar />
-      <div className="px-20 py-20 flex flex-row gap-4">
-        <div className="w-1/2">
-          <ProductGallery />
+      <CategoriesButtons />
+      <div className="bg-gray-50">
+        <div className="px-20">
+          <div className="flex flex-row items-center gap-2 py-4 text-neutral-400">
+            <p>Home</p>
+            <RiArrowRightSLine className="h-4 w-4" />
+            <p>Laptops</p>
+          </div>
         </div>
-        <div className="p-2 w-1/2">
-          <ProductTitle />
+        <div className="px-20">
+          <div className="h-full p-4 rounded-md border border-sky-300 bg-white grid grid-cols-2 gap-4">
+            <ProductGallery />
+            <ProductTitle />
+          </div>
+        </div>
+
+        <div className="px-20 mt-6 bg-gray-50">
+          <Tab.Group>
+            <Tab.List>
+              <Tab>Tab 1</Tab>
+              <Tab>Tab 2</Tab>
+              <Tab>Tab 3</Tab>
+            </Tab.List>
+            <Tab.Panels>
+              <Tab.Panel>Content 1</Tab.Panel>
+              <Tab.Panel>Content 2</Tab.Panel>
+              <Tab.Panel>Content 3</Tab.Panel>
+            </Tab.Panels>
+          </Tab.Group>
+          <ProductDescription />
         </div>
       </div>
 
