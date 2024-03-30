2974c89122a73c286ae97db6d6d420a4d7048cf3
kibet <kmishmael@gmail.com>
2024-03-30 12:21:14 +0300

    chore: personal data page

--- client/app/customer/notification/page.tsx
+++ client/app/customer/notification/page.tsx
@@ -1,3 +1,121 @@
+import { Direct, Truck, Sms, Story } from "iconsax-react";
 export default function Page() {
-    
+  return (
+    <>
+      <div className="p-2">
+        <h2 className="font-semibold text-xl">Notification</h2>
+        <h4 className="font-normal text-sm py-2 text-neutral-600">
+          Manage your notification settings
+        </h4>
+        <br />
+
+        <div className="grid grid-cols-2 gap-5">
+          <div className="flex flex-row gap-2 justify-between">
+            <div className="flex flex-col gap-2">
+              <div className="flex font-semibold items-center flex-row gap-5">
+                <Direct className="h-5 w-5 font-bold" />
+                <p>Emails</p>
+              </div>
+              <p className="text-xs text-neutral-500">
+                We write emails to let you know what is important, like: new
+                order, confirmations
+              </p>
+            </div>
+            <div className="">
+              <label className="inline-flex items-center cursor-pointer">
+                <input
+                  type="checkbox"
+                  value=""
+                  className="sr-only peer"
+                  checked
+                />
+                <div className="relative w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+                <span className="ms-3 hidden text-sm font-medium text-gray-900 dark:text-gray-300">
+                  Checked toggle
+                </span>
+              </label>
+            </div>
+          </div>
+          <div className="flex flex-row gap-2 justify-between">
+            <div className="flex flex-col gap-2">
+              <div className="flex font-semibold items-center flex-row gap-5">
+                <Truck className="h-5 w-5 font-bold" />
+                <p>Order Delivered</p>
+              </div>
+              <p className="text-xs text-neutral-500">
+                You will be notified once the order is delivered
+              </p>
+            </div>
+            <div className="">
+              <label className="inline-flex items-center cursor-pointer">
+                <input
+                  type="checkbox"
+                  value=""
+                  className="sr-only peer"
+                  checked
+                />
+                <div className="relative w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+                <span className="ms-3 hidden text-sm font-medium text-gray-900 dark:text-gray-300">
+                  Checked toggle
+                </span>
+              </label>
+            </div>
+          </div>
+
+          <div className="flex flex-row gap-2 justify-between">
+            <div className="flex flex-col gap-2">
+              <div className="flex font-semibold items-center flex-row gap-5">
+                <Sms className="h-5 w-5 font-bold" />
+                <p>Push to your Device</p>
+              </div>
+              <p className="text-xs text-neutral-500">
+                Receive notifications about your order status, promotions and
+                other updates
+              </p>
+            </div>
+            <div className="">
+              <label className="inline-flex items-center cursor-pointer">
+                <input
+                  type="checkbox"
+                  value=""
+                  className="sr-only peer"
+                  checked
+                />
+                <div className="relative w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+                <span className="ms-3 hidden text-sm font-medium text-gray-900 dark:text-gray-300">
+                  Checked toggle
+                </span>
+              </label>
+            </div>
+          </div>
+
+          <div className="flex flex-row gap-2 justify-between">
+            <div className="flex flex-col gap-2">
+              <div className="flex font-semibold items-center flex-row gap-5">
+                <Story className="h-5 w-5 font-bold" />
+                <p>Product&apos;s availability</p>
+              </div>
+              <p className="text-xs text-neutral-500">
+                You will be notified when product
+               gets available</p>
+            </div>
+            <div className="">
+              <label className="inline-flex items-center cursor-pointer">
+                <input
+                  type="checkbox"
+                  value=""
+                  className="sr-only peer"
+                  checked
+                />
+                <div className="relative w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+                <span className="ms-3 hidden text-sm font-medium text-gray-900 dark:text-gray-300">
+                  Checked toggle
+                </span>
+              </label>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
--- client/app/customer/security-access/page.tsx
+++ client/app/customer/security-access/page.tsx
@@ -1,3 +1,39 @@
+import { Edit, Key, Call } from "iconsax-react";
+
 export default function Page() {
-    
+  return (
+    <>
+      <div className="p-2">
+        <h2 className="font-semibold text-xl">Security settings</h2>
+        <h4 className="font-normal text-sm py-2 text-neutral-600">
+          Change password and phone number
+        </h4>
+        <br />
+        {/* <Suspense>
+          <PersonalDataDialog />
+        </Suspense> */}
+
+        <div className="grid grid-cols-2 gap-5">
+          <div className="flex flex-col gap-2">
+            <button className="flex flex-row py-4 px-4 items-center justify-between rounded-md bg-gray-100">
+              <div className="flex flex-row gap-3 items-center text-neutral-700">
+                <Key size={20} variant="Outline" />
+                <p className="">Password</p>
+              </div>
+              <Edit className="text-primary" size="24" variant="Outline" />
+            </button>
+          </div>
+          <div className="flex flex-col gap-2">
+            <button className="flex flex-row py-4 px-4 items-center justify-between rounded-md bg-gray-100">
+              <div className="flex flex-row gap-3 items-center text-neutral-700">
+                <Call size={20} variant="Outline" />
+                <p className="">Phone number</p>
+              </div>
+              <Edit className="text-primary" size="24" variant="Outline" />
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
