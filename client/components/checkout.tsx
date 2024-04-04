d70b528a116173fde9807f111ea5c4d7be619f46
kibet <kmishmael@gmail.com>
2024-04-04 13:29:19 +0300

    fix testing out payment method

--- client/components/checkout.tsx
+++ client/components/checkout.tsx
@@ -1,34 +1,42 @@
 "use client";
 import Axios from "axios";
 import { useSession } from "next-auth/react";
-import { useRouter } from 'next/navigation'
+import { useRouter } from "next/navigation";
+const IntaSend = require("intasend-inlinejs-sdk");
 
 export default function Checkout({ amount }: { amount: number }) {
   const session = useSession();
 
   const router = useRouter();
 
-  Axios.post("ISPubKey_live_59238a95-d19c-478c-9ee6-3c8615e8240b", {
-    public_key: "ISPubKey_test_146fe58b-5b1c-46aa-8af8-1d4b308569f8",
-    first_name: session.data?.user.name?.split(" ")[0],
-    last_name: session.data?.user.name?.split(" ")[1],
-    email: session.data?.user.email,
-    phone_number: amount,
-    host: "https://civrot.com",
-    amount,
-    currency: "USD",
-  })
-    .then((res) => {
-      router.push(res.data.url);
-      console.log(res);
+  // Axios.post("https://sandbox.intasend.com/api/v1/checkout/", {
+  //   public_key: "ISPubKey_live_59238a95-d19c-478c-9ee6-3c8615e8240b",
+  //   first_name: session.data?.user.name?.split(" ")[0],
+  //   last_name: session.data?.user.name?.split(" ")[1],
+  //   email: session.data?.user.email,
+  //   phone_number: amount,
+  //   amount,
+  //   currency: "USD",
+  // })
+  //   .then((res) => {
+  //     router.push(res.data.url);
+  //     console.log(res);
+  //   })
+  //   .catch((error) => {
+  //     console.log(error);
+  //   });
+
+    new IntaSend({
+      publicAPIKey: "ISPubKey_live_59238a95-d19c-478c-9ee6-3c8615e8240b",
+      live: true //or true for live environment
     })
-    .catch((error) => {
-      console.log(error);
-    });
+    .on("COMPLETE", (response: any) => { console.log("COMPLETE:", response) })
+    .on("FAILED", (response: any) => { console.log("FAILED", response) })
+    .on("IN-PROGRESS", () => { console.log("INPROGRESS ...") })
 
   return (
     <>
-      <button className="text-center font-semibold text-sm w-full py-2 px-4 rounded-md bg-primary hover:bg-sky-800 text-white">
+      <button data-amount="10" data-currency="USD" className="intaSendPayButton text-center font-semibold text-sm w-full py-2 px-4 rounded-md bg-primary hover:bg-sky-800 text-white">
         Continue to pay
       </button>
     </>
