89e827747a536d541983a2c9c0369ce1e573d9d0
kmishmael <kmishmael@gmail.com>
2024-07-12 14:35:54 +0300

    fix: category button invalid links

--- client/components/categories-buttons.tsx
+++ client/components/categories-buttons.tsx
@@ -5,7 +5,7 @@ export default function CategoriesButtons({ categories }: { categories: any }) {
   return (
     <div className="border-t flex gap-3 border-b py-2">
       {categories.map((d: any) => (
-        <Link href={`/category/{d.slug}`} key={d.slug}>
+        <Link href={`/category/${d.slug}`} key={d.slug}>
           <ButtonDrop title={d.name} />
         </Link>
       ))}
